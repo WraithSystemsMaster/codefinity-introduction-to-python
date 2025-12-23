0c9b0f13e9fc2db331ecd4f7c0b405f80059ae95
WraithSystemsMaster <>
2025-12-23 04:30:45 +0000

    Tuple Operations

---
+++ other_data_types/tuple_operations/main.py
@@ -0,0 +1,15 @@
+# Initial items on shelf #1 (provided as a tuple)
+shelf1 = ("celery", "spinach", "cucumbers")
+
+# Items being added to the shelf #1 (provided as a list)
+shelf1_update = ["tomatoes", "celery", "cilantro"]
+
+#Convert to tuples
+shelf1_update_tuple= (shelf1_update)
+
+
+
+#Output
+print(f"Updated Shelf #1:")
+print(f"Number of Celery:")
+print(f"Celery Index:")
